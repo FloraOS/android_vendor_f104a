c549851ec6794d1c509021cbe406e39e7a959c3c
f104a <f104a@f104a.io>
2024-11-25 11:03:08 +0100

    [meta] [version]: bump version to beta3
    Change-Id: I9de1507a4db3f0d5b1e92be539d33fc545d9f4e8

--- version.mk
+++ version.mk
@@ -1,7 +1,7 @@
 # FloraOS version information
 FLORAOS_CODENAME   := mentha
 FLORAOS_VERSION    := 1.0
-FLORAOS_REVISION   := beta2
+FLORAOS_REVISION   := beta3
 
 # Build number from file
 BUILD_DATE := $(shell date -u +%Y%m%d.%H%M%S)
