8dc4336c04b08c7e8709a54a24cba7134a421c17
f104a <f104a@f104a.io>
2024-12-01 23:16:03 +0100

    [security] [common]: optionally include security configurations
    Change-Id: I69fdcaa31c3eaefaed52cf7e9ccca0e3f462aa66

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,7 @@
 # Include all configuration files
 include vendor/f104a/config/packages.mk
 include vendor/f104a/config/go.mk
+
+# Private security configurations
+# As they are private they are optionally included
+-include vendor/f104a/security/common.mk
