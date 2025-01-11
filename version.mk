5b9ed2c72e308886a046f87017e59a51a8ef7ee0
f104a <f104a@f104a.io>
2025-01-12 00:45:07 +0100

    [meta] [version]: bump version to 1.1beta1
    Change-Id: Ib261c2d47685d346ccaa9a6e8650b074784c9ad4

--- version.mk
+++ version.mk
@@ -1,7 +1,7 @@
 # FloraOS version information
 FLORAOS_CODENAME   := zingiber
 FLORAOS_VERSION    := 1.1
-FLORAOS_REVISION   := alpha2
+FLORAOS_REVISION   := beta1
 
 # Build number from file
 BUILD_DATE := $(shell date -u +%Y%m%d.%H%M%S)
